951c72f921511ff10645b966d11ed25de02ce37d
Audrow Nash <audrow@hey.com>
2022-05-10 17:27:31 -0700

    1.2.0
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- rqt_bag_plugins/setup.py
+++ rqt_bag_plugins/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_bag_plugins'
 setup(
     name=package_name,
-    version='1.1.2',
+    version='1.2.0',
     package_dir={'': 'src'},
     packages=['rqt_bag_plugins'],
     data_files=[
