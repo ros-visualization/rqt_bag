b5e3761d93f7d722a403d88aefb507b8cee484bf
Emerson Knapp <emerson.b.knapp@gmail.com>
2022-09-26 16:37:43 -0700

    Use rosbag2_py.reader for all message queries, remove sqlite3 direct usage
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- rqt_bag/src/rqt_bag/rosbag2.py
+++ rqt_bag/src/rqt_bag/rosbag2.py
@@ -36,7 +36,6 @@
 from collections import namedtuple
 import os
 import pathlib
-import sqlite3
 
 from rclpy.clock import Clock, ClockType
 from rclpy.duration import Duration
@@ -48,6 +47,8 @@ from rosidl_runtime_py.utilities import get_message
 import yaml
 
 WRITE_ONLY_MSG = "open for writing only, returning None"
+
+Entry = namedtuple('Entry', ['topic', 'data', 'timestamp'])
 
 
 class Rosbag2:
@@ -73,6 +74,9 @@ class Rosbag2:
                 t_info.topic_metadata.name: t_info.topic_metadata
                 for t_info in self.metadata.topics_with_message_count
             }
+            self.read_order = rosbag2_py.ReadOrder(
+                sort_by=rosbag2_py.ReadOrderSortBy.ReceivedTimestamp,
+                reverse=False)
 
     def size(self):
         """Get the size of the rosbag."""
@@ -120,29 +124,41 @@ class Rosbag2:
         if not self.reader:
             self._logger.warn("get_entry - " + WRITE_ONLY_MSG)
             return None
-        sql_query = 'timestamp<={} ORDER BY messages.timestamp ' \
-                    'DESC LIMIT 1;'.format(timestamp.nanoseconds)
-        result = self._execute_sql_query(sql_query, topic)
-        return result[0] if result else None
+
+        self.reader.set_read_order(rosbag2_py.ReadOrder(reverse=True))
+        self.reader.seek(timestamp.nanoseconds)
+        return self.read_next() if self.reader.has_next() else None
 
     def get_entry_after(self, timestamp, topic=None):
         """Get the next entry after a given timestamp."""
         if not self.reader:
             self._logger.warn("get_entry_after - " + WRITE_ONLY_MSG)
             return None
-        sql_query = 'timestamp>{} ORDER BY messages.timestamp ' \
-                    'LIMIT 1;'.format(timestamp.nanoseconds)
-        result = self._execute_sql_query(sql_query, topic)
-        return result[0] if result else None
+
+        self.reader.set_read_order(rosbag2_py.ReadOrder(reverse=False))
+        self.reader.seek(timestamp.nanoseconds)
+        return self.read_next() if self.reader.has_next() else None
 
     def get_entries_in_range(self, t_start, t_end, topic=None):
         if not self.reader:
             self._logger.warn("get_entries_in_range - " + WRITE_ONLY_MSG)
             return None
-        """Get a list of all of the entries within a given range of timestamps (inclusive)."""
-        sql_query = 'timestamp>={} AND timestamp<={} ' \
-                    'ORDER BY messages.timestamp;'.format(t_start.nanoseconds, t_end.nanoseconds)
-        return self._execute_sql_query(sql_query, topic)
+
+        self.reader.set_read_order(rosbag2_py.ReadOrder(reverse=False))
+        self.reader.set_filter(rosbag2_py.StorageFilter(topics=[topic] if topic else []))
+        self.reader.seek(t_start.nanoseconds)
+        entries = []
+        while self.reader.has_next():
+            next_entry = self.read_next()
+            if next_entry.timestamp <= t_end.nanoseconds:
+                entries.append(next_entry)
+            else:
+                break
+
+        return entries
+
+    def read_next(self):
+        return Entry(*self.reader.read_next())
 
     def deserialize_entry(self, entry):
         """Deserialize a bag entry into its corresponding ROS message."""
@@ -150,20 +166,3 @@ class Rosbag2:
         msg_type = get_message(msg_type_name)
         ros_message = deserialize_message(entry.data, msg_type)
         return (ros_message, msg_type_name, entry.topic)
-
-    def _execute_sql_query(self, sql_query, topic):
-        Entry = namedtuple('Entry', ['topic', 'data', 'timestamp'])
-        base_query = 'SELECT topics.name, data, timestamp FROM messages ' \
-                     'JOIN topics ON messages.topic_id = topics.id WHERE '
-
-        # If there was a topic requested, make sure it is in this bag
-        if topic is not None:
-            if topic not in self.topic_metadata_map:
-                return []
-            base_query += 'topics.name="{}"AND '.format(topic)
-
-        with sqlite3.connect(self.db_name) as db:
-            cursor = db.cursor()
-            entries = cursor.execute(base_query + sql_query).fetchall()
-            cursor.close()
-            return [Entry(*entry) for entry in entries]
