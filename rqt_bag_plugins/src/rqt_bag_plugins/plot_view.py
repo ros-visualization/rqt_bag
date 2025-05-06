b720ebb61af5fc6599e4d6ffad3402c4ae598401
Martin Pecka <peckama2@fel.cvut.cz>
2025-05-07 00:51:25 +0200

    Improved handling of arrays in plot_view
    Signed-off-by: Martin Pecka <peckama2@fel.cvut.cz>

--- rqt_bag_plugins/src/rqt_bag_plugins/plot_view.py
+++ rqt_bag_plugins/src/rqt_bag_plugins/plot_view.py
@@ -60,12 +60,15 @@
 #  doesn't make sense to make it align with the timeline. This could be done
 #  if someone wanted to implement a separate timeline view
 
+import array
 import codecs
 import math
 import os
 import threading
 
 from ament_index_python import get_resource
+
+import numpy
 
 from python_qt_binding import loadUi
 from python_qt_binding.QtCore import Qt, qWarning
@@ -81,6 +84,9 @@ from rqt_bag import bag_helper
 from rqt_bag import MessageView
 
 from rqt_plot.data_plot import DataPlot
+
+MAX_LIST_LEN = 50
+LIST_TAIL_LEN = 10
 
 
 class PlotView(MessageView):
@@ -432,18 +438,28 @@ class MessageTree(QTreeWidget):
         if hasattr(obj, '_fields_and_field_types'):
             field_keys = obj.get_fields_and_field_types().keys()
             subobjs = [(field_name, getattr(obj, field_name)) for field_name in field_keys]
-        elif type(obj) in [list, tuple]:
-            len_obj = len(obj)
+        elif type(obj) in (list, tuple, array.array, numpy.ndarray):
+            if type(obj) in (array.array, numpy.ndarray):
+                list_obj = obj.tolist()
+            else:
+                list_obj = obj
+            len_obj = len(list_obj)
+            short_list_obj = list_obj[:MAX_LIST_LEN]
+
             if len_obj == 0:
                 subobjs = []
             else:
                 w = int(math.ceil(math.log10(len_obj)))
-                subobjs = [('[%*d]' % (w, i), subobj) for (i, subobj) in enumerate(obj)]
+                subobjs = [('[%*d]' % (w, i), subobj) for (i, subobj) in enumerate(short_list_obj)]
+                if len_obj > MAX_LIST_LEN:
+                    for i in range(-LIST_TAIL_LEN, 0):
+                        if len_obj + i >= MAX_LIST_LEN:
+                            subobjs.append(('[%*d]' % (w, len_obj + i), list_obj[i]))
         else:
             subobjs = []
 
         plotitem = False
-        if type(obj) in [int, float]:
+        if type(obj) in (int, float):
             plotitem = True
             if isinstance(obj, float):
                 obj_repr = '%.6f' % obj
