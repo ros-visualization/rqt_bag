d3d0eb5d4ae5797a13ddc31934f8f3985fd83ee5
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-18 19:04:39 +0200

    2.2.5

--- rqt_bag_plugins/setup.py
+++ rqt_bag_plugins/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_bag_plugins'
 setup(
     name=package_name,
-    version='2.2.4',
+    version='2.2.5',
     package_dir={'': 'src'},
     packages=['rqt_bag_plugins'],
     data_files=[
